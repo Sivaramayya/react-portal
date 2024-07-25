81e8677c31905156e724b6685403b253efe8d0e0
saripillisireesha <95239362+saripillisireesha@users.noreply.github.com>
2024-07-25 01:32:50 -0400

    Update Welcome.js

--- src/login/Welcome.js
+++ src/login/Welcome.js
@@ -1,6 +1,6 @@
 import React, { useContext,useState,useEffect } from 'react';
 import { UserContext } from '../context/UserContext';
-import { Link } from 'react-router-dom';
+
 import axios from 'axios';
 const Welcome = () => {
   const { user, orderItemsLength, orderItems } = useContext(UserContext);
