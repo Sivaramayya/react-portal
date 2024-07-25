41f511a87e83820777293698f84b053a2f5fcf6e
divyanjalimadhupada <155824889+divyanjaliMadhupada08@users.noreply.github.com>
2024-07-25 15:35:47 +0530

    updated login

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,41 +1,36 @@
 import React, { useState, useContext } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate,Link } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
 import axios from 'axios';
 
 class User {
-  constructor(email, password, userId,name) {
+  constructor(name, phoneNumber, email, password, address,userId) {
+    this.name = name;
+    this.phoneNumber = phoneNumber;
     this.email = email;
     this.password = password;
+    this.address = address;
     this.userId = userId;
-    this.name=name;
   }
 }
-
 const Login = () => {
   const [loginUser, setLoginUser] = useState({ email: '', password: '' });
-  const { setUser, setOrderItemsLength, setOrderItems } = useContext(UserContext);
-  const navigate = useNavigate();
+  const { setUser } = useContext(UserContext); // Accessing context
+  const navigate = useNavigate();  // Routing
 
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
       const response = await axios.post('http://localhost:9191/user-management/users/login', {
-     
         email: loginUser.email,
         password: loginUser.password
       });
-      
       console.log(response.data);
-      const orderItems = response.data.orderItems;
-      const orderItemsLength = response.data.orderItems.length;
-      setOrderItemsLength(orderItemsLength);
-      setOrderItems(orderItems);
-      const user = new User(response.data.email, response.data.password, response.data.userId, response.data.name);
-      setUser(user);
+      const user = new User(response.data.name,response.data.phoneNumber,response.data.email, response.data.password, response.data.address,response.data.userId); 
+      setUser(user); // Update context
       navigate("/welcome");
     } catch (err) {
-      console.error(err);
+      console.error(err); 
       navigate("/login-failed");
     }
   };
@@ -49,6 +44,7 @@ const Login = () => {
           value={loginUser.email}
           onChange={(e) => setLoginUser({ ...loginUser, email: e.target.value })}
           placeholder="Enter username"
+
           required />
       </label>
       <label>
@@ -60,11 +56,12 @@ const Login = () => {
           placeholder="Enter password"
           required />
       </label>
+      
+      
       <button type="submit">Login</button>
 
     </form>
     <p>New User? <Link to="/register"><button type="submit">Register</button></Link></p></>
-
   );
 };
 
