63c11b1044d3d3213646219ed548a4c7458e356f
divyanjalimadhupada <155824889+divyanjaliMadhupada08@users.noreply.github.com>
2024-09-24 21:42:10 +0530

    created react-portal repo

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,9 +1,10 @@
 // UserContext.js
 import React, { createContext, useState } from 'react';
 
-export const UserContext = createContext();
+export const UserContext = createContext();//Create Context
 
-export const UserProvider = ({ children }) => {
+export const UserProvider = ({ children }) => {  
+  //Initial Values in the starting of the context
   const [user, setUser] = useState(null);
   const [orderItemsLength, setOrderItemsLength] = useState(0);
   const [orderItems, setOrderItems] = useState([]);
